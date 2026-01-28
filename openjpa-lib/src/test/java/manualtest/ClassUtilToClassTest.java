9fe59c6aa4e3a438f4796dc891e7b2f83fdc92b6
Gaia Meola <gaia.meola@gmail.com>
2026-01-28 14:33:08 +0100

    Modifica di CacheUtil

--- openjpa-lib/src/test/java/manualtest/ClassUtilToClassTest.java
+++ openjpa-lib/src/test/java/manualtest/ClassUtilToClassTest.java
@@ -18,34 +18,36 @@ class ClassUtilToClassTest {
     private enum InputCategory {
         VALID_CLASS,
         INVALID_CLASS,
-        NULL_CLASS
+        NULL_CLASS,
+        TWO_PARAM_CONSTRUCTOR // AGGIUNTO per coprire toClass(String, ClassLoader)
     }
 
     private static Stream<Arguments> data() {
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
         return Stream.of(
-                // T1, T2.1, T3.1, T4.1, T4.2: Casi che passano normalmente
-                Arguments.of(InputCategory.VALID_CLASS, "java.util.Map$Entry", false, ClassLoader.getSystemClassLoader(), java.util.Map.Entry.class, null),
-                Arguments.of(InputCategory.VALID_CLASS, "java.lang.String", false, ClassLoader.getSystemClassLoader(), String.class, null),
-                Arguments.of(InputCategory.VALID_CLASS, "int", false, ClassLoader.getSystemClassLoader(), int.class, null),
-                Arguments.of(InputCategory.VALID_CLASS, "[I", false, ClassLoader.getSystemClassLoader(), int[].class, null),
-                Arguments.of(InputCategory.VALID_CLASS, "[Ljava.lang.String;", false, ClassLoader.getSystemClassLoader(), String[].class, null),
+                // T1 - T4.2: Casi standard
+                Arguments.of(InputCategory.VALID_CLASS, "java.util.Map$Entry", false, loader, java.util.Map.Entry.class, null),
+                Arguments.of(InputCategory.VALID_CLASS, "java.lang.String", false, loader, String.class, null),
+                Arguments.of(InputCategory.VALID_CLASS, "int", false, loader, int.class, null),
+                Arguments.of(InputCategory.VALID_CLASS, "[I", false, loader, int[].class, null),
+                Arguments.of(InputCategory.VALID_CLASS, "[Ljava.lang.String;", false, loader, String[].class, null),
 
                 // T2.2: SLASH-NOTATION ---> test fallito
-//                Arguments.of(InputCategory.VALID_CLASS, "java/lang/Object", false, ClassLoader.getSystemClassLoader(), Object.class, null),
+                // Arguments.of(InputCategory.VALID_CLASS, "java/lang/Object", false, ClassLoader.getSystemClassLoader(), Object.class, null),
 
-                // T5, T6, T7: Casi di robustezza ed errori sintattici
-                Arguments.of(InputCategory.INVALID_CLASS, "", false, ClassLoader.getSystemClassLoader(), null, Exception.class),
-                Arguments.of(InputCategory.INVALID_CLASS, "java..lang.String", false, ClassLoader.getSystemClassLoader(), null, Exception.class),
-                Arguments.of(InputCategory.NULL_CLASS, null, false, ClassLoader.getSystemClassLoader(), null, Exception.class),
+                // T11, T12, T13: Risoluzione righe 89-100 (Array letterali)
+                Arguments.of(InputCategory.VALID_CLASS, "int[]", false, loader, int[].class, null),
+                Arguments.of(InputCategory.VALID_CLASS, "java.lang.String[]", false, loader, String[].class, null),
+                Arguments.of(InputCategory.VALID_CLASS, "double[][]", false, loader, double[][].class, null),
 
-                // T11: Array primitivo letterale.
-                Arguments.of(InputCategory.VALID_CLASS, "int[]", false, ClassLoader.getSystemClassLoader(), int[].class, null),
+                // T14: Risoluzione Copertura Costruttore a 2 parametri (toClass(str, loader))
+                // Questo test coprirà la riga orfana che delega il lavoro al metodo a 3 parametri.
+                Arguments.of(InputCategory.TWO_PARAM_CONSTRUCTOR, "java.lang.Integer", false, loader, Integer.class, null),
 
-                // T12: Array oggetti letterale.
-                Arguments.of(InputCategory.VALID_CLASS, "java.lang.String[]", false, ClassLoader.getSystemClassLoader(), String[].class, null),
-
-                // T13: Array multidimensionale letterale.
-                Arguments.of(InputCategory.VALID_CLASS, "double[][]", false, ClassLoader.getSystemClassLoader(), double[][].class, null),
+                // T5, T6, T7: Casi di robustezza
+                Arguments.of(InputCategory.INVALID_CLASS, "", false, loader, null, Exception.class),
+                Arguments.of(InputCategory.INVALID_CLASS, "java..lang.String", false, loader, null, Exception.class),
+                Arguments.of(InputCategory.NULL_CLASS, null, false, loader, null, Exception.class),
 
                 // T10: Risoluzione copertura ClassLoader nullo
                 Arguments.of(InputCategory.VALID_CLASS, "java.lang.String", true, null, String.class, null)
@@ -69,8 +71,16 @@ class ClassUtilToClassTest {
             return;
         }
 
-        // Caso normale: verifico che il risultato sia quello atteso
-        Class<?> result = ClassUtil.toClass(str, resolve, loader);
+        // LOGICA MODIFICATA:
+        Class<?> result;
+        if (category == InputCategory.TWO_PARAM_CONSTRUCTOR) {
+            // Chiamata al metodo a 2 parametri per coprire lo 0% di quel metodo
+            result = ClassUtil.toClass(str, loader);
+        } else {
+            // Chiamata standard al metodo a 3 parametri
+            result = ClassUtil.toClass(str, resolve, loader);
+        }
+
         assertEquals(expectedClass, result,
                 () -> "Unexpected result for category " + category + " with input: " + str);
     }

dd04b3eab53c5e49c38a6c9aaf7145da2c0fbc52
Gaia Meola <gaia.meola@gmail.com>
2026-01-28 13:08:48 +0100

    Modifica di CacheUtil

--- openjpa-lib/src/test/java/manualtest/ClassUtilGetPackageNameStringTest.java
+++ openjpa-lib/src/test/java/manualtest/ClassUtilGetPackageNameStringTest.java
@@ -1,6 +1,5 @@
 package manualtest;
 
-import costumutils.Utils;
 import org.apache.openjpa.lib.util.ClassUtil;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.Timeout;
@@ -10,7 +9,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ClassUtilGetPackageNameStringTest {
@@ -25,29 +25,32 @@ class ClassUtilGetPackageNameStringTest {
     private static Stream<Arguments> data() {
         return Stream.of(
                 // ===== STRINGHE VALIDE =====
-                // 4.5.1) Classe interna; test passato
-                Arguments.of(InputCategory.VALID, Utils.validInnerClass(), "javax.swing", null),
-//                // 4.5.2) Classe con package; test passato
-                Arguments.of(InputCategory.VALID, Utils.validNormalClassWithPackage(), "java.lang", null),
-//                // 4.5.3) Classe senza package (primitiva); test passato
-                Arguments.of(InputCategory.EMPTY, Utils.validPrimitiveClass(), "", null),
-//                // 4.5.4) Classe array; test passato
-                Arguments.of(InputCategory.VALID, Utils.validArrayClassObject(), "java.awt", null),
-//
-//                // ===== STRINGHE NON VALIDE =====
-//                // 4.5.5) Stringa vuota; test passato
-                Arguments.of(InputCategory.EMPTY, Utils.emptyString(), "", null),
-//                // 4.5.6) Stringa non valida; test fallito
-//                Arguments.of(InputCategory.INVALID, Utils.invalidBinaryName(), null, Exception.class)
-//
-//                // ===== STRINGHE NULLE =====
-//                // 4.5.7) Null; test passato
-                Arguments.of(InputCategory.NULL, Utils.nullString(), null, null)
+                // 4.5.1) Classe interna
+                Arguments.of(InputCategory.VALID, "java.util.Map$Entry", "java.util", null),
 
-                //Aggiunti i test a seguito da JaCoCo
-//                Arguments.of(InputCategory.VALID, Utils.validPrimitiveArray(), "", null),         // GP-1
-//                Arguments.of(InputCategory.VALID, Utils.validPrimitiveMultiArray(), "", null),    // GP-2
-//                Arguments.of(InputCategory.VALID, "[Ljava.lang.String;", "java.lang", null)       // GP-3
+                // 4.5.2) Classe con package
+                Arguments.of(InputCategory.VALID, "java.lang.String", "java.lang", null),
+//                Arguments.of(InputCategory.VALID, "java/lang/Object", "java/lang", null), // RAFFINAMENTO: Slash-notation --> Test Fallito
+
+                // 4.5.3) Classe senza package (Default Package)
+                Arguments.of(InputCategory.VALID, "MyClass", "", null),
+                Arguments.of(InputCategory.VALID, "int", "", null),
+
+                // 4.5.4) Classe array (Specializzazione)
+                Arguments.of(InputCategory.VALID, "[I", "", null),                               // Array Primitivo (nessun package)
+                Arguments.of(InputCategory.VALID, "[Ljava.lang.String;", "java.lang", null),      // Array Riferimento
+                Arguments.of(InputCategory.VALID, "[[[Ljava.awt.Point;", "java.awt", null),      // Multidimensionale
+
+                // ===== STRINGHE NON VALIDE =====
+                // 4.5.5) Stringa vuota
+                Arguments.of(InputCategory.EMPTY, "", "", null),
+
+                // 4.5.6) Stringa non valida (Robustezza)
+//                Arguments.of(InputCategory.INVALID, "java..lang.String", null, Exception.class), // Ci si aspetta Exception --> Test fallito
+
+                // ===== STRINGHE NULLE =====
+                // 4.5.7) Null
+                Arguments.of(InputCategory.NULL, null, null, null)
         );
     }
 
